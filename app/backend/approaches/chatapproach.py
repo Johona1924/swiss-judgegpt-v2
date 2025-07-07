3d115dcc2949b43feb8aaf2fbd2fcdb3ddf9e2ed
jruepp <jonas.ruepp@bluewin.ch>
2025-07-07 12:19:55 +0200

    support for multiple tool calls

--- app/backend/approaches/chatapproach.py
+++ app/backend/approaches/chatapproach.py
@@ -3,6 +3,7 @@ import re
 from abc import ABC, abstractmethod
 from collections.abc import AsyncGenerator, Awaitable
 from typing import Any, Optional, Union, cast
+import logging
 
 from openai import AsyncStream
 from openai.types.chat import (
@@ -30,16 +31,23 @@ class ChatApproach(Approach, ABC):
     def get_search_query(self, chat_completion: ChatCompletion, user_query: str):
         response_message = chat_completion.choices[0].message
 
+        search_queries = []
+
         if response_message.tool_calls:
             for tool in response_message.tool_calls:
                 if tool.type != "function":
                     continue
                 function = tool.function
-                if function.name == "search_sources":
+                if function.name.startswith("search_sources"):
                     arg = json.loads(function.arguments)
                     search_query = arg.get("search_query", self.NO_RESPONSE)
                     if search_query != self.NO_RESPONSE:
-                        return search_query
+                        search_queries.append(search_query.strip())
+            if search_queries:
+                return " | ".join(search_queries)
+            else:
+                logging.DEBUG(f"tool_calls detected, but parsed empty list : {search_queries}")
+                return user_query
         elif query_text := response_message.content:
             if query_text.strip() != self.NO_RESPONSE:
                 return query_text
